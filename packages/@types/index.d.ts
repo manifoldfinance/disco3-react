3b0294da58eb053024c57dbabe1e645d560512a2
sam bacha <sam@manifoldfinance.com>
2022-01-03 13:49:35 -0800

    chore(repo): cleanup

--- packages/@types/index.d.ts
+++
@@ -1,4 +0,0 @@
-// @since v0.0.8
-declare module '*.png'
-declare module 'window'
-declare const global: typeof globalThis & { window: CustomWindow }
--- packages/empty/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Actions } from '@disco3/types';
-import { Connector } from '@disco3/types';
-export declare class Empty extends Connector {
-    constructor(actions: Actions);
-    activate(): Promise<void>;
-}
