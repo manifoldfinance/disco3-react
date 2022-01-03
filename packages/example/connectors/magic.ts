49d75a021677c4db94e90a7a50df3c57fb8d724f
sam bacha <sam@manifoldfinance.com>
2022-01-03 13:44:39 -0800

    build(turbo): refactor build env

--- packages/example/connectors/magic.ts
+++
@@ -1,9 +0,0 @@
-import { initializeConnector } from '@disco3/core';
-import { Magic } from '@disco3/magic';
-
-export const [magic, hooks] = initializeConnector<Magic>(
-  (actions) =>
-    new Magic(actions, {
-      apiKey: process.env.magicKey,
-    }),
-);
--- packages/example/connectors/metaMask.ts
+++
@@ -1,4 +0,0 @@
-import { initializeConnector } from '@disco3/core';
-import { MetaMask } from '@disco3/metamask';
-
-export const [metaMask, hooks] = initializeConnector<MetaMask>((actions) => new MetaMask(actions));
