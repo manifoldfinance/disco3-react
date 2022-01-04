a2300eeabba8a0a173c2e1a542fead67b47ee2ff
sam bacha <sam@manifoldfinance.com>
2022-01-03 21:24:41 -0800

    chore(lint): format

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 # [disco3](https://manifoldfinance.com)
 
-> a performant web3 react library 
+> a performant web3 react library
 
-[![build - passing](https://img.shields.io/badge/build-passing-black)](https://) [![manifoldfinance.eth](https://img.shields.io/static/v1?label=&message=manifoldfinance.eth&color=black&logo=ethereum&logoColor=white)](https://etherscan.io/enslookup-search?search=manifoldfinance.eth) [![0.8.0](https://img.shields.io/static/v1?label=&message=0.8.0&color=black&logo=solidity&logoColor=white)](https://etherscan.io/enslookup-search?search=manifoldfinance.eth) [![manifoldfinance - disco3-react](https://img.shields.io/static/v1?label=manifoldfinance&message=disco3-react&color=black&logo=github)](https://github.com/manifoldfinance/disco3-react "Go to GitHub repo")
-
-
+[![build - passing](https://img.shields.io/badge/build-passing-black)](https://)
+[![manifoldfinance.eth](https://img.shields.io/static/v1?label=&message=manifoldfinance.eth&color=black&logo=ethereum&logoColor=white)](https://etherscan.io/enslookup-search?search=manifoldfinance.eth)
+[![0.8.0](https://img.shields.io/static/v1?label=&message=0.8.0&color=black&logo=solidity&logoColor=white)](https://etherscan.io/enslookup-search?search=manifoldfinance.eth)
+[![manifoldfinance - disco3-react](https://img.shields.io/static/v1?label=manifoldfinance&message=disco3-react&color=black&logo=github)](https://github.com/manifoldfinance/disco3-react 'Go to GitHub repo')
 
 ## Documentation
 
@@ -18,11 +19,10 @@
 
 This turborepo has some additional tools already setup for you:
 
-
-
 ## Setup
 
-This repository is used in the `npx create-turbo` command, and selected when choosing which package manager you wish to use with your monorepo (Yarn).
+This repository is used in the `npx create-turbo` command, and selected when choosing which package
+manager you wish to use with your monorepo (Yarn).
 
 ### Build
 
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 import type { Actions, Connector, Web3ReactState } from '@disco3/types';
-import create, { UseBoundStore } from 'zustand'
-import { useEffect, useMemo, useState } from 'react'
+import create, { UseBoundStore } from 'zustand';
+import { useEffect, useMemo, useState } from 'react';
 
 import type { Networkish } from '@ethersproject/networks';
 import { Web3Provider } from '@ethersproject/providers';
@@ -8,132 +8,137 @@ import { createWeb3ReactStoreAndActions } from '@disco3/store';
 
 export type Web3ReactHooks = ReturnType<typeof getStateHooks> &
   ReturnType<typeof getDerivedHooks> &
-  ReturnType<typeof getAugmentedHooks>
+  ReturnType<typeof getAugmentedHooks>;
 
 export function initializeConnector<T extends Connector>(
   f: (actions: Actions) => T,
-  allowedChainIds?: number[]
+  allowedChainIds?: number[],
 ): [T, Web3ReactHooks] {
-  const [store, actions] = createWeb3ReactStoreAndActions(allowedChainIds)
+  const [store, actions] = createWeb3ReactStoreAndActions(allowedChainIds);
 
-  const connector = f(actions)
-  const useConnector = create<Web3ReactState>(store)
+  const connector = f(actions);
+  const useConnector = create<Web3ReactState>(store);
 
-  const stateHooks = getStateHooks(useConnector)
-  const derivedHooks = getDerivedHooks(stateHooks)
+  const stateHooks = getStateHooks(useConnector);
+  const derivedHooks = getDerivedHooks(stateHooks);
 
-  const augmentedHooks = getAugmentedHooks(connector, stateHooks, derivedHooks)
+  const augmentedHooks = getAugmentedHooks(connector, stateHooks, derivedHooks);
 
-  return [connector, { ...stateHooks, ...derivedHooks, ...augmentedHooks }]
+  return [connector, { ...stateHooks, ...derivedHooks, ...augmentedHooks }];
 }
 
-const CHAIN_ID = (state: Web3ReactState) => state.chainId
-const ACCOUNTS = (state: Web3ReactState) => state.accounts
-const ACTIVATING = (state: Web3ReactState) => state.activating
-const ERROR = (state: Web3ReactState) => state.error
+const CHAIN_ID = (state: Web3ReactState) => state.chainId;
+const ACCOUNTS = (state: Web3ReactState) => state.accounts;
+const ACTIVATING = (state: Web3ReactState) => state.activating;
+const ERROR = (state: Web3ReactState) => state.error;
 
 function getStateHooks(useConnector: UseBoundStore<Web3ReactState>) {
   function useChainId(): Web3ReactState['chainId'] {
-    return useConnector(CHAIN_ID)
+    return useConnector(CHAIN_ID);
   }
 
   function useAccounts(): Web3ReactState['accounts'] {
-    return useConnector(ACCOUNTS)
+    return useConnector(ACCOUNTS);
   }
 
   function useIsActivating(): Web3ReactState['activating'] {
-    return useConnector(ACTIVATING)
+    return useConnector(ACTIVATING);
   }
 
   function useError(): Web3ReactState['error'] {
-    return useConnector(ERROR)
+    return useConnector(ERROR);
   }
 
-  return { useChainId, useAccounts, useIsActivating, useError }
+  return { useChainId, useAccounts, useIsActivating, useError };
 }
 
-function getDerivedHooks({ useChainId, useAccounts, useIsActivating, useError }: ReturnType<typeof getStateHooks>) {
+function getDerivedHooks({
+  useChainId,
+  useAccounts,
+  useIsActivating,
+  useError,
+}: ReturnType<typeof getStateHooks>) {
   function useAccount(): string | undefined {
-    return useAccounts()?.[0]
+    return useAccounts()?.[0];
   }
 
   function useIsActive(): boolean {
-    const chainId = useChainId()
-    const accounts = useAccounts()
-    const activating = useIsActivating()
-    const error = useError()
+    const chainId = useChainId();
+    const accounts = useAccounts();
+    const activating = useIsActivating();
+    const error = useError();
 
-    return Boolean(chainId && accounts && !activating && !error)
+    return Boolean(chainId && accounts && !activating && !error);
   }
 
-  return { useAccount, useIsActive }
+  return { useAccount, useIsActive };
 }
 
 function useENS(provider?: Web3Provider, accounts?: string[]): (string | null)[] | undefined {
-  const [ENSNames, setENSNames] = useState<(string | null)[] | undefined>()
+  const [ENSNames, setENSNames] = useState<(string | null)[] | undefined>();
 
   useEffect(() => {
     if (provider && accounts?.length) {
-      let stale = false
+      let stale = false;
 
       Promise.all(accounts.map((account) => provider.lookupAddress(account)))
         .then((ENSNames) => {
           if (!stale) {
-            setENSNames(ENSNames)
+            setENSNames(ENSNames);
           }
         })
         .catch((error) => {
-          console.debug('Could not fetch ENS names', error)
-        })
+          console.debug('Could not fetch ENS names', error);
+        });
 
       return () => {
-        stale = true
-        setENSNames(undefined)
-      }
+        stale = true;
+        setENSNames(undefined);
+      };
     }
-  }, [provider, accounts])
+  }, [provider, accounts]);
 
-  return ENSNames
+  return ENSNames;
 }
 
 function getAugmentedHooks<T extends Connector>(
   connector: T,
   { useChainId, useAccounts, useError }: ReturnType<typeof getStateHooks>,
-  { useAccount, useIsActive }: ReturnType<typeof getDerivedHooks>
+  { useAccount, useIsActive }: ReturnType<typeof getDerivedHooks>,
 ) {
   function useProvider(network?: Networkish): Web3Provider | undefined {
-    const isActive = useIsActive()
+    const isActive = useIsActive();
 
-    const chainId = useChainId()
-    const accounts = useChainId()
+    const chainId = useChainId();
+    const accounts = useChainId();
 
     return useMemo(() => {
       // we use chainId and accounts to re-render in case connector.provider changes in place
       if (isActive && connector.provider && chainId && accounts) {
-        return new Web3Provider(connector.provider, network)
+        return new Web3Provider(connector.provider, network);
       }
-    }, [isActive, network, chainId, accounts])
+    }, [isActive, network, chainId, accounts]);
   }
 
   function useENSNames(provider: Web3Provider | undefined): (string | null)[] | undefined {
-    const accounts = useAccounts()
+    const accounts = useAccounts();
 
-    return useENS(provider, accounts)
+    return useENS(provider, accounts);
   }
 
   function useENSName(provider: Web3Provider | undefined): (string | null) | undefined {
-    const account = useAccount()
+    const account = useAccount();
 
-    return useENS(provider, account === undefined ? undefined : [account])?.[0]
+    return useENS(provider, account === undefined ? undefined : [account])?.[0];
   }
 
   // for backwards compatibility only
   function useWeb3React(provider: Web3Provider | undefined) {
-    const chainId = useChainId()
-    const error = useError()
+    const chainId = useChainId();
+    const error = useError();
 
-    const account = useAccount()
-    const isActive = useIsActive()
+    const account = useAccount();
+    const isActive = useIsActive();
 
     return {
       connector,
@@ -142,8 +147,8 @@ function getAugmentedHooks<T extends Connector>(
       account,
       active: isActive,
       error,
-    }
+    };
   }
 
-  return { useProvider, useENSNames, useENSName, useWeb3React }
+  return { useProvider, useENSNames, useENSName, useWeb3React };
 }
