3451515bf511dd876af3d4e9d407e216ba0868c7
sam bacha <sam@manifoldfinance.com>
2022-02-17 16:37:57 -0800

    fix(web3): refactor connectors

--- packages/eip1193/src/index.spec.ts
+++ packages/eip1193/src/index.spec.ts
@@ -11,158 +11,158 @@ import { EventEmitter } from 'node:events';
 import { EIP1193 } from '.';
 
 export async function yieldThread() {
-  await new Promise((resolve) => setTimeout(resolve, 0));
+  await new Promise((resolve) => setTimeout(resolve, 0))
 }
 
 class MockProviderRpcError extends Error {
-  public code: number;
+  public code: number
   constructor() {
-    super('Mock Provider RPC Error');
-    this.code = 4200;
+    super('Mock Provider RPC Error')
+    this.code = 4200
   }
 }
 
 export class MockEIP1193Provider extends EventEmitter {
-  public chainId?: string;
-  public accounts?: string[];
+  public chainId?: string
+  public accounts?: string[]
 
-  public eth_chainId = jest.fn((chainId?: string) => chainId);
-  public eth_accounts = jest.fn((accounts?: string[]) => accounts);
-  public eth_requestAccounts = jest.fn((accounts?: string[]) => accounts);
+  public eth_chainId = jest.fn((chainId?: string) => chainId)
+  public eth_accounts = jest.fn((accounts?: string[]) => accounts)
+  public eth_requestAccounts = jest.fn((accounts?: string[]) => accounts)
 
   public request(x: RequestArguments): Promise<unknown> {
     switch (x.method) {
       case 'eth_chainId':
-        return Promise.resolve(this.eth_chainId(this.chainId));
+        return Promise.resolve(this.eth_chainId(this.chainId))
       case 'eth_accounts':
-        return Promise.resolve(this.eth_accounts(this.accounts));
+        return Promise.resolve(this.eth_accounts(this.accounts))
       case 'eth_requestAccounts':
-        return Promise.resolve(this.eth_requestAccounts(this.accounts));
+        return Promise.resolve(this.eth_requestAccounts(this.accounts))
       default:
-        throw new Error();
+        throw new Error()
     }
   }
 
   public emitConnect(chainId: string) {
-    this.emit('connect', { chainId });
+    this.emit('connect', { chainId })
   }
 
   public emitDisconnect(error: ProviderRpcError) {
-    this.emit('disconnect', error);
+    this.emit('disconnect', error)
   }
 
   public emitChainChanged(chainId: string) {
-    this.emit('chainChanged', chainId);
+    this.emit('chainChanged', chainId)
   }
 
   public emitAccountsChanged(accounts: string[]) {
-    this.emit('accountsChanged', accounts);
+    this.emit('accountsChanged', accounts)
   }
 }
 
-const chainId = '0x1';
-const accounts: string[] = [];
+const chainId = '0x1'
+const accounts: string[] = []
 
 describe('EIP1193', () => {
-  let mockProvider: MockEIP1193Provider;
+  let mockProvider: MockEIP1193Provider
 
-  let store: Web3ReactStore;
-  let actions: Actions;
+  let store: Web3ReactStore
+  let actions: Actions
 
-  let connector: EIP1193;
+  let connector: EIP1193
 
   beforeEach(() => {
-    mockProvider = new MockEIP1193Provider();
-    [store, actions] = createWeb3ReactStoreAndActions();
-  });
+    mockProvider = new MockEIP1193Provider()
+    ;[store, actions] = createWeb3ReactStoreAndActions()
+  })
 
   describe('ethers', () => {
     afterEach(() => {
-      expect(mockProvider.eth_chainId.mock.calls.length).toBe(1);
-      expect(mockProvider.eth_accounts.mock.calls.length).toBe(0);
-      expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(1);
-    });
+      expect(mockProvider.eth_chainId.mock.calls.length).toBe(1)
+      expect(mockProvider.eth_accounts.mock.calls.length).toBe(0)
+      expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(1)
+    })
 
     test('works', async () => {
-      mockProvider.chainId = chainId;
-      mockProvider.accounts = accounts;
+      mockProvider.chainId = chainId
+      mockProvider.accounts = accounts
 
-      const web3Provider = new Web3Provider(mockProvider);
-      const wrapped = new Eip1193Bridge(web3Provider.getSigner(), web3Provider);
+      const web3Provider = new Web3Provider(mockProvider)
+      const wrapped = new Eip1193Bridge(web3Provider.getSigner(), web3Provider)
 
-      connector = new EIP1193(actions, wrapped, false);
+      connector = new EIP1193(actions, wrapped, false)
 
-      await connector.activate();
+      await connector.activate()
 
       expect(store.getState()).toEqual({
         chainId: 1,
         accounts,
         activating: false,
         error: undefined,
-      });
-    });
-  });
+      })
+    })
+  })
 
   describe('functions', () => {
     describe('connectEagerly = true', () => {
       beforeEach(() => {
-        expect(mockProvider.eth_chainId.mock.calls.length).toBe(0);
-        expect(mockProvider.eth_accounts.mock.calls.length).toBe(0);
-        expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(0);
-      });
+        expect(mockProvider.eth_chainId.mock.calls.length).toBe(0)
+        expect(mockProvider.eth_accounts.mock.calls.length).toBe(0)
+        expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(0)
+      })
 
       afterEach(() => {
-        expect(mockProvider.eth_chainId.mock.calls.length).toBe(1);
-        expect(mockProvider.eth_accounts.mock.calls.length).toBe(1);
-        expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(0);
-      });
+        expect(mockProvider.eth_chainId.mock.calls.length).toBe(1)
+        expect(mockProvider.eth_accounts.mock.calls.length).toBe(1)
+        expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(0)
+      })
 
       // suppress console.debugs in this block
       beforeEach(() => {
-        jest.spyOn(console, 'debug').mockImplementation(() => {});
-      });
+        jest.spyOn(console, 'debug').mockImplementation(() => {})
+      })
       afterEach(() => {
-        jest.clearAllMocks();
-      });
+        jest.clearAllMocks()
+      })
 
       test('fails silently', async () => {
-        connector = new EIP1193(actions, mockProvider);
-        await yieldThread();
+        connector = new EIP1193(actions, mockProvider)
+        await yieldThread()
 
         expect(store.getState()).toEqual({
           chainId: undefined,
           accounts: undefined,
           activating: false,
           error: undefined,
-        });
-      });
+        })
+      })
 
       test('succeeds', async () => {
-        mockProvider.chainId = chainId;
-        mockProvider.accounts = accounts;
+        mockProvider.chainId = chainId
+        mockProvider.accounts = accounts
 
-        connector = new EIP1193(actions, mockProvider);
-        await yieldThread();
+        connector = new EIP1193(actions, mockProvider)
+        await yieldThread()
 
         expect(store.getState()).toEqual({
           chainId: 1,
           accounts,
           activating: false,
           error: undefined,
-        });
-      });
-    });
+        })
+      })
+    })
 
     describe('connectEagerly = false', () => {
       beforeEach(() => {
-        connector = new EIP1193(actions, mockProvider, false);
-      });
+        connector = new EIP1193(actions, mockProvider, false)
+      })
 
       beforeEach(() => {
-        expect(mockProvider.eth_chainId.mock.calls.length).toBe(0);
-        expect(mockProvider.eth_accounts.mock.calls.length).toBe(0);
-        expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(0);
-      });
+        expect(mockProvider.eth_chainId.mock.calls.length).toBe(0)
+        expect(mockProvider.eth_accounts.mock.calls.length).toBe(0)
+        expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(0)
+      })
 
       test('before', () => {
         expect(store.getState()).toEqual({
@@ -170,167 +170,165 @@ describe('EIP1193', () => {
           accounts: undefined,
           activating: false,
           error: undefined,
-        });
-      });
+        })
+      })
 
       describe('#activate', () => {
         afterEach(() => {
-          expect(mockProvider.eth_chainId.mock.calls.length).toBe(1);
-          expect(mockProvider.eth_accounts.mock.calls.length).toBe(0);
-          expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(1);
-        });
+          expect(mockProvider.eth_chainId.mock.calls.length).toBe(1)
+          expect(mockProvider.eth_accounts.mock.calls.length).toBe(0)
+          expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(1)
+        })
 
         test(`chainId = ${chainId}`, async () => {
-          mockProvider.chainId = chainId;
-          mockProvider.accounts = accounts;
+          mockProvider.chainId = chainId
+          mockProvider.accounts = accounts
 
-          await connector.activate();
+          await connector.activate()
 
           expect(store.getState()).toEqual({
             chainId: 1,
             accounts,
             activating: false,
             error: undefined,
-          });
-        });
+          })
+        })
 
         test("chainId = '0x01'", async () => {
-          mockProvider.chainId = '0x01';
-          mockProvider.accounts = accounts;
+          mockProvider.chainId = '0x01'
+          mockProvider.accounts = accounts
 
-          await connector.activate();
+          await connector.activate()
 
           expect(store.getState()).toEqual({
             chainId: 1,
             accounts,
             activating: false,
             error: undefined,
-          });
-        });
+          })
+        })
 
         test("accounts = ['0x0000000000000000000000000000000000000000']", async () => {
-          const chainId = '0x1';
-          const accounts: string[] = [
-            '0x0000000000000000000000000000000000000000',
-          ];
+          const chainId = '0x1'
+          const accounts: string[] = ['0x0000000000000000000000000000000000000000']
 
-          mockProvider.chainId = chainId;
-          mockProvider.accounts = accounts;
+          mockProvider.chainId = chainId
+          mockProvider.accounts = accounts
 
-          await connector.activate();
+          await connector.activate()
 
           expect(store.getState()).toEqual({
             chainId: 1,
             accounts,
             activating: false,
             error: undefined,
-          });
-        });
+          })
+        })
 
         test("accounts = ['0x0000000000000000000000000000000000000000', '0x0000000000000000000000000000000000000001']", async () => {
-          const chainId = '0x1';
+          const chainId = '0x1'
           const accounts: string[] = [
             '0x0000000000000000000000000000000000000000',
             '0x0000000000000000000000000000000000000001',
-          ];
+          ]
 
-          mockProvider.chainId = chainId;
-          mockProvider.accounts = accounts;
+          mockProvider.chainId = chainId
+          mockProvider.accounts = accounts
 
-          await connector.activate();
+          await connector.activate()
 
           expect(store.getState()).toEqual({
             chainId: 1,
             accounts,
             activating: false,
             error: undefined,
-          });
-        });
-      });
-    });
-  });
+          })
+        })
+      })
+    })
+  })
 
   describe('events', () => {
     beforeEach(() => {
-      connector = new EIP1193(actions, mockProvider, false);
-    });
+      connector = new EIP1193(actions, mockProvider, false)
+    })
 
     afterEach(() => {
-      expect(mockProvider.eth_chainId.mock.calls.length).toBe(0);
-      expect(mockProvider.eth_accounts.mock.calls.length).toBe(0);
-      expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(0);
-    });
+      expect(mockProvider.eth_chainId.mock.calls.length).toBe(0)
+      expect(mockProvider.eth_accounts.mock.calls.length).toBe(0)
+      expect(mockProvider.eth_requestAccounts.mock.calls.length).toBe(0)
+    })
 
-    const chainId = '0x1';
-    const accounts: string[] = [];
-    const error = new MockProviderRpcError();
+    const chainId = '0x1'
+    const accounts: string[] = []
+    const error = new MockProviderRpcError()
 
     test('#connect', async () => {
-      mockProvider.emitConnect(chainId);
+      mockProvider.emitConnect(chainId)
 
       expect(store.getState()).toEqual({
         chainId: 1,
         accounts: undefined,
         activating: false,
         error: undefined,
-      });
-    });
+      })
+    })
 
     test('#disconnect', async () => {
-      mockProvider.emitDisconnect(error);
+      mockProvider.emitDisconnect(error)
 
       expect(store.getState()).toEqual({
         chainId: undefined,
         accounts: undefined,
         activating: false,
         error,
-      });
-    });
+      })
+    })
 
     test('#chainChanged', async () => {
-      mockProvider.emitChainChanged(chainId);
+      mockProvider.emitChainChanged(chainId)
 
       expect(store.getState()).toEqual({
         chainId: 1,
         accounts: undefined,
         activating: false,
         error: undefined,
-      });
-    });
+      })
+    })
 
     test('#accountsChanged', async () => {
-      mockProvider.emitAccountsChanged(accounts);
+      mockProvider.emitAccountsChanged(accounts)
 
       expect(store.getState()).toEqual({
         chainId: undefined,
         accounts,
         activating: false,
         error: undefined,
-      });
-    });
+      })
+    })
 
     test('initializes with connect', async () => {
-      mockProvider.emitConnect(chainId);
-      mockProvider.emitAccountsChanged(accounts);
+      mockProvider.emitConnect(chainId)
+      mockProvider.emitAccountsChanged(accounts)
 
       expect(store.getState()).toEqual({
         chainId: 1,
         accounts,
         activating: false,
         error: undefined,
-      });
-    });
+      })
+    })
 
     test('initializes with chainChanged', async () => {
-      mockProvider.emitChainChanged(chainId);
-      mockProvider.emitAccountsChanged(accounts);
+      mockProvider.emitChainChanged(chainId)
+      mockProvider.emitAccountsChanged(accounts)
 
       expect(store.getState()).toEqual({
         chainId: 1,
         accounts,
         activating: false,
         error: undefined,
-      });
-    });
-  });
-});
+      })
+    })
+  })
+})
--- packages/walletconnect/src/index.spec.ts
+++ packages/walletconnect/src/index.spec.ts
@@ -10,54 +10,50 @@ import { MockEIP1193Provider } from '../../eip1193/src/index.spec';
 // necessary because walletconnect returns chainId as a number
 export class MockMockWalletConnectProvider extends MockEIP1193Provider {
   public eth_chainId_number = jest.fn((chainId?: string) =>
-    chainId === undefined ? chainId : Number.parseInt(chainId, 16),
-  );
+    chainId === undefined ? chainId : Number.parseInt(chainId, 16)
+  )
 
   public request(x: RequestArguments): Promise<unknown> {
     if (x.method === 'eth_chainId') {
-      return Promise.resolve(this.eth_chainId_number(this.chainId));
+      return Promise.resolve(this.eth_chainId_number(this.chainId))
     } else {
-      return super.request(x);
+      return super.request(x)
     }
   }
 }
 
-jest.mock(
-  '@walletconnect/ethereum-provider',
-  () => MockMockWalletConnectProvider,
-);
+jest.mock('@walletconnect/ethereum-provider', () => MockMockWalletConnectProvider)
 
-const chainId = '0x1';
-const accounts: string[] = [];
+const chainId = '0x1'
+const accounts: string[] = []
 
 describe('WalletConnect', () => {
-  let store: Web3ReactStore;
-  let connector: WalletConnect;
-  let mockConnector: MockMockWalletConnectProvider;
+  let store: Web3ReactStore
+  let connector: WalletConnect
+  let mockConnector: MockMockWalletConnectProvider
 
   describe('connectEagerly = true', () => {
     beforeEach(() => {
-      let actions: Actions;
-      [store, actions] = createWeb3ReactStoreAndActions();
-      connector = new WalletConnect(actions, {});
-    });
+      let actions: Actions
+      ;[store, actions] = createWeb3ReactStoreAndActions()
+      connector = new WalletConnect(actions, {})
+    })
 
     beforeEach(async () => {
-      mockConnector =
-        connector.provider as unknown as MockMockWalletConnectProvider;
-      mockConnector.chainId = chainId;
-      mockConnector.accounts = accounts;
-    });
+      mockConnector = connector.provider as unknown as MockMockWalletConnectProvider
+      mockConnector.chainId = chainId
+      mockConnector.accounts = accounts
+    })
 
     test('#activate', async () => {
-      await connector.activate();
+      await connector.activate()
 
       expect(store.getState()).toEqual({
         chainId: Number.parseInt(chainId, 16),
         accounts,
         activating: false,
         error: undefined,
-      });
-    });
-  });
-});
+      })
+    })
+  })
+})
