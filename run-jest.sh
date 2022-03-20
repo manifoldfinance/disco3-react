cfa09f9c9a7aaea854c5e7c41a0fa1307b5b4708
sam bacha <sam@manifoldfinance.com>
2022-03-19 20:08:26 -0700

    test(jest): expose gc

--- run-jest.sh
+++ run-jest.sh
@@ -3,8 +3,17 @@
 # cd to the root of the repo
 cd "$(git rev-parse --show-toplevel)"
 
-# avoids re-compilation during publishing of both packages
-if [[ ! -v CI ]]; then
-  yarn run install
+# Start in tasks/ even if run from root directory
+cd "$(dirname "$0")"
+
+set -x
+
+yarn install
+yarn run build
+node --expose-gc node_modules/.bin/jest --coverage --globals "{\"coverage\":true}" packages/
+
+if [ -n "$CI" ]; then
+  jestArgs="${jestArgs} --maxWorkers=4 --ci"
 fi
-./node_modules/.bin/jest packages/
+
+$node "$(yarn bin jest)" $jestArgs
