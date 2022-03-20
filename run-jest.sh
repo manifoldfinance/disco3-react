034a9ecccad43a1dc1ac6d8e452023b37f9e0258
sam bacha <sam@manifoldfinance.com>
2022-03-19 23:02:30 -0700

    fix(runner): env

--- run-jest.sh
+++ run-jest.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
-# cd to the root of the repo
-cd "$(git rev-parse --show-toplevel)"
-
-# Start in tasks/ even if run from root directory
-cd "$(dirname "$0")"
-
-set -x
-
-yarn install
-yarn run build
+set -o errexit
+echo "Starting jest testing..."
 node --expose-gc node_modules/.bin/jest --coverage --globals "{\"coverage\":true}" packages/
-
-if [ -n "$CI" ]; then
-  jestArgs="${jestArgs} --maxWorkers=4 --ci"
-fi
-
-$node "$(yarn bin jest)" $jestArgs
+sleep 1
+echo "Process script terminated"
